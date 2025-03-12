4ec557e7ef018e5100558e5015b75832a6cecb17
Phuong <ndpp.work@gmail.com>
2025-03-12 08:18:22 +0700

    phuongndp/dtb/update-dtb

---
+++ build/web/WEB-INF/classes/util/QueryArtist.sql
@@ -0,0 +1,10 @@
+INSERT INTO Artists ([Name], Followers, ArtistUrl)  
+VALUES 
+    (N'Sơn Tùng M-TP', 5000000, 'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/avatars/5/9/6/9/59696c9dba7a914d587d886049c10df6.jpg'),
+    (N'Big Bang', 1200000, 'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/avatars/2/2/3/3/223327eb3eca92cc50d58aaf985f17f3.jpg'),
+	(N'Dương Domic', 1200000, 'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/avatars/c/9/7/9/c97982b6ae9f7a52196be520f6f25d46.jpg'),
+	(N'RHYDER', 1200000, 'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/avatars/5/c/1/f/5c1fa6ef95fe2f63044eaca0a11d304b.jpg'),
+	(N'AMEE', 1200000, 'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/avatars/d/b/8/2/db82811d8db60180f8251866fd2ede7d.jpg'),
+	(N'ERIK', 1200000, 'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/avatars/6/e/8/b/6e8bc06300770de22c3a56db89100a4c.jpg'),
+	(N'SZA', 1200000, 'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/avatars/2/7/9/a/279a391af4107610977011d29d5f7cec.jpg'),
+    (N'Alan Walker', 2500000, 'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/avatars/b/a/6/b/ba6b56ca02dd06f37693d945acfcdad9.jpg');
---
+++ src/java/util/SongQuery.sql
@@ -0,0 +1,42 @@
+INSERT INTO Songs ([Name], Streams, Likes, SongUrl, ThumbnailUrl, ArtistId)  
+VALUES 
+    (N'Nơi Này Có Anh', 25000000, 1200000, 'https://a128-z3.zmdcdn.me/d17d79105dfcbf67208389c9e8fc9c42?authen=exp=1741710290~acl=/d17d79105dfcbf67208389c9e8fc9c42*~hmac=90304ff497a0055d5d55740226f4addc', 'https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/covers/3/a/3a9e48bc4df7bbde3acea30cc267f609_1487066528.jpg', 1),
+    (N'Muộn Rồi Mà Sao Còn', 20000000, 1100000, 'https://a128-z3.zmdcdn.me/ed296514b07543c8f29162802af28948?authen=exp=1741710853~acl=/ed296514b07543c8f29162802af28948*~hmac=11991d147504c692730f33981266822a', '#', 1),
+    (N'Hãy Trao Cho Anh', 30000000, 1500000, 'https://a128-z3.zmdcdn.me/593b54ec5c26d2a70585377cf9374578?authen=exp=1741710688~acl=/593b54ec5c26d2a70585377cf9374578*~hmac=bae7b8c2670fe39e3a7de843b83dbeb0', '#', 1),
+    (N'Có Chắc Yêu Là Đây', 18000000, 900000, 'https://a128-z3.zmdcdn.me/2bb97c1386402292968c225793647c77?authen=exp=1741711150~acl=/2bb97c1386402292968c225793647c77*~hmac=dd213a24b41e1bedc15a22e46133465f', '#', 1),
+    (N'Chúng Ta Của Hiện Tại', 22000000, 1000000, 'https://a128-z3.zmdcdn.me/9d5e0023aeae573dc1000abec75de7d1?authen=exp=1741711296~acl=/9d5e0023aeae573dc1000abec75de7d1*~hmac=e6d04360e7566689dec5eb35a5040975', '#', 1),
+
+    (N'Still Life', 27000000, 1400000, '#', '#', 2),
+    (N'Haru Haru', 30000000, 1600000, '#', '#', 2),
+    (N'BANG BANG BANG', 35000000, 1800000, '#', '#', 2),
+    (N'IF YOU', 28000000, 1300000, '#', '#', 2),
+
+    (N'Chập Chờn', 15000000, 700000, '#', '#', 3),
+    (N'Tràn Bộ Nhớ', 12000000, 650000, '#', '#', 3),
+    (N'Pin Dự Phòng', 10000000, 500000, '#', '#', 3),
+    (N'Mất Kết Nối', 13000000, 550000, '#', '#', 3),
+
+    (N'Nỗi Đau Đính Kèm', 18000000, 850000, '#', '#', 4),
+    (N'Chịu Cách Mình Nói Thua', 14000000, 600000, '#', '#', 4),
+    (N'Dân Chơi Sao Phải Khóc', 19000000, 900000, '#', '#', 4),
+
+    (N'MỘNG YU', 20000000, 950000, '#', '#', 5),
+    (N'Cuộc Gọi Lúc Nửa Đêm', 17000000, 800000, '#', '#', 5),
+    (N'Beautiful Nightmare (Interlude)', 16000000, 750000, '#', '#', 5),
+    (N'Miền Mộng Mị', 15000000, 700000, '#', '#', 5),
+    (N'2000 Câu Hỏi Vì Sao', 13000000, 650000, '#', '#', 5),
+
+    (N'Có Tất Cả Nhưng Thiếu Anh', 25000000, 1200000, '#', '#', 6),
+    (N'Chạm Đáy Nỗi Đau', 22000000, 1000000, '#', '#', 6),
+    (N'Sau Tất Cả', 20000000, 950000, '#', '#', 6),
+    (N'Mùa Đông', 15000000, 700000, '#', '#', 6),
+
+    (N'Better Off (Alone, Pt. III)', 18000000, 850000, '#', '#', 7),
+    (N'Hero', 16000000, 750000, '#', '#', 7),
+    (N'Endless Summer', 14000000, 600000, '#', '#', 7),
+
+    (N'Kill Bill', 25000000, 1300000, '#', '#', 8),
+    (N'SOS', 30000000, 1500000, '#', '#', 8),
+    (N'LOW', 22000000, 1000000, '#', '#', 8),
+    (N'Blind', 20000000, 950000, '#', '#', 8),
+    (N'Nobody Gets Me', 18000000, 900000, '#', '#', 8);
